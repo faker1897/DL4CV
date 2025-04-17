a3115fc4441d6c2c840f6bf5579bc7608640bd78
faker1897 <yangyunsong040529@163.com>
2025-04-17 12:47:30 -0700

    commit all dataset

--- classification.py
+++ classification.py
@@ -59,6 +59,17 @@ train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 # Standardize the data.
+data_augmentation = keras.Sequential(
+  [
+    layers.RandomFlip("horizontal",
+                      input_shape=(img_height,
+                                  img_width,
+                                  3)),
+    layers.RandomRotation(0.1),
+    layers.RandomZoom(0.1),
+  ]
+)
+
 normalization_layer = layers.Rescaling(1./255)
 
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
@@ -71,16 +82,19 @@ print(np.min(first_image), np.max(first_image))
 num_classes = len(class_names)
 
 model = Sequential([
-  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  data_augmentation,
+  layers.Rescaling(1./255),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
+    # add Dropout
+  layers.Dropout(0.2),
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
+  layers.Dense(num_classes, name="outputs")
 ])
 
 # compile model
@@ -92,7 +106,7 @@ model.compile(optimizer='adam',
 model.summary()
 
 # train model
-epochs=10
+epochs = 15
 history = model.fit(
   train_ds,
   validation_data=val_ds,
